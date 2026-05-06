22b8435c0a8c06ee18f1d2af47710f870858a83e
brownquartz <parkcm2262@gmail.com>
2026-05-06 18:20:15 +0900

    feat: add post-it style favicon

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -5,6 +5,8 @@ export default function Document() {
     <Html lang="ja" className="dark">
       <Head>
         <meta name="google-site-verification" content="5hVaEwa5iqGLgl8f-AW3-cEoFsg7zxBqHyka6rQow6c" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
       </Head>
       <body>
         <Main />
