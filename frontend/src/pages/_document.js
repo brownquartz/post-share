c74726fde7d767652cee2a6e4a267fa40cc12904
brownquartz <parkcm2262@gmail.com>
2026-05-06 14:47:10 +0900

    feat: add Google Search Console verification meta tag

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ja" className="dark">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="google-site-verification=5hVaEwa5iqGLgl8f-AW3-cEoFsg7zxBqHyka6rQow6c" />
+      </Head>
       <body>
         <Main />
         <NextScript />
